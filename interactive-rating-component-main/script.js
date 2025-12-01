c055ad27c6334e4221e5ea70d39590f877d77e54
Abbos <79392566+shomurodovabbos@users.noreply.github.com>
2025-12-01 19:36:19 +0500

    Add files via upload

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,60 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const container = document.body;
+    container.innerHTML = `<div class="cart cart-submit">
+            <div class="cart_logo">
+                <img src="./images/icon-star.svg" alt="star" />
+            </div>
+            <div class="cart_content">
+                <h1>How did we do?</h1>
+                <p>
+                    Please let us know how we did with your support request. All
+                    feedback is appreciated to help us improve our offering!
+                </p>
+            </div>
+            <div class="cart_form">
+                <div class="cart_form--numbers">
+                    <button class="number-btn">1</button>
+                    <button class="number-btn">2</button>
+                    <button class="number-btn">3</button>
+                    <button class="number-btn">4</button>
+                    <button class="number-btn">5</button>
+                </div>
+                <button class="submit-btn">Submit</button>
+            </div>
+        </div>
+        <div class="cart cart-complete">
+            
+        </div>`;
+    const numberBtns = document.querySelectorAll(".number-btn");
+    const submitBtn = document.querySelector(".submit-btn");
+    const cartSubmit = document.querySelector(".cart-submit");
+    const cartComplete = document.querySelector(".cart-complete");
+    let selectedIndex = null;
+    numberBtns.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            selectedIndex = index + 1;
+            const activeBtn = document.querySelector(".number-btn.active");
+            if (activeBtn) {
+                activeBtn.classList.remove("active");
+            }
+            btn.classList.add("active");
+        });
+    });
+    submitBtn.addEventListener("click", () => {
+        cartSubmit.style.display = "none";
+        cartComplete.classList.add("active");
+        cartComplete.innerHTML = `<div class="cart-complete_logo">
+                <img src="./images/illustration-thank-you.svg" alt="logo" />
+            </div>
+
+             <h3>You selected ${selectedIndex}  out of 5</h3>
+
+            <div class="cart-complete_content">
+                <h1>Thank you</h1>
+                <p>
+                    We appreciate you taking the time to give a rating. If you
+                    ever need more support, don’t hesitate to get in touch!
+                </p>
+            </div>`;
+    });
+});
---
+++ interactive-rating-component-main/style-guide.md
@@ -0,0 +1,36 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+-   Mobile: 375px
+-   Desktop: 1440px
+
+> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
+
+## Colors
+
+### Primary
+
+-   Orange 500: #fb7413
+
+### Neutral
+
+-   White: #ffffff
+-   Grey 500: #959eac
+-   Grey 900: #252d37
+-   Grey 950: #121417
+
+## Typography
+
+### Body Copy
+
+-   Font size (paragraph): 15px
+
+### Font
+
+-   Family: [Overpass](https://fonts.google.com/specimen/Overpass)
+-   Weights: 400, 700
+
+> 💎 [Upgrade to Pro](https://www.frontendmentor.io/pro?ref=style-guide) for design file access to see all design details and get hands-on experience using a professional workflow with tools like Figma.
